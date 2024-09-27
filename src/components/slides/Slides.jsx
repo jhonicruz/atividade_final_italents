845f15228952162f315ee1a5fa6fd73c489275bc
Jhoni Cruz <jhonidevpro@gmail.com>
2024-09-27 18:15:19 -0300

    🦄 refactor(carrousel):
    fix bug imagens, and add new images to carrousel

--- src/components/slides/Slides.jsx
+++ src/components/slides/Slides.jsx
@@ -1,5 +1,10 @@
+/* eslint-disable no-unused-vars */
 import React from 'react';
 import Hero from '../../assets/image1.png';
+import Img1 from '../../assets/img1.jpg';
+import Img2 from '../../assets/img2.jpg';
+import Img3 from '../../assets/img3.jpg';
+import Img4 from '../../assets/img4.jpg';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
@@ -10,10 +15,26 @@ import 'swiper/css/pagination';
 import 'swiper/css/autoplay';
 
 const Slides = () => {
-  const slides = ['img1, img2', 'img3', 'img4', 'img5'];
+  const slides = [
+    {
+      src: Hero,
+    },
+    {
+      src: Img1,
+    },
+    {
+      src: Img2,
+    },
+    {
+      src: Img3,
+    },
+    {
+      src: Img4,
+    },
+  ];
 
   return (
-    <div className=" flex bg-zinc-n1 w-full h-full justify-center pt-[60px]">
+    <div className=" flex bg-zinc-n1 w-full justify-center h-[400px] pt-[60px] bg-red-400">
       <Swiper
         spaceBetween={50}
         pagination={{ clickable: true }}
@@ -23,10 +44,10 @@ const Slides = () => {
         onSwiper={() => console.log()}
         modules={[Autoplay, Pagination, Navigation]}
       >
-        {slides.map((slide) => {
+        {slides.map((slide, index) => {
           return (
-            <SwiperSlide key={slide}>
-              <img src={Hero} alt="" className="w-full object-cover" />
+            <SwiperSlide key={index} className="bg-teal-300 w-ful h-full flex ">
+              <img src={slide.src} alt="" className="w-full object-cover" />
             </SwiperSlide>
           );
         })}
